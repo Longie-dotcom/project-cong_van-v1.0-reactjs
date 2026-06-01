dd90e20345a600594e2b30d33d1bf0d96efd5599
Long Dong <longdong32120@gmail.com>
2026-06-01 21:02:20 +0700

    update

--- cong-van/src/data/config.js
+++ cong-van/src/data/config.js
@@ -2,7 +2,7 @@ export const GAME_BALANCE = {
   // Tỉ lệ mỗi lần click vào mỏ
   MINE_CLICK: {
     BASE_COAL: 2,         // Than cơ bản nếu chưa có nhân lực hoặc nhân lực âm!
-    MULTIPLIER: 0.5,    // Hệ số nhân với RESOURCE (nhân lực), Ví dụ: MULTIPLIER = 0.1 thì tức là 10 nhân lực sẽ cho ra 1 than cho mỗi click, note: Đã làm tròn trong công thức
+    MULTIPLIER: 0.321,    // Hệ số nhân với RESOURCE (nhân lực), Ví dụ: MULTIPLIER = 0.1 thì tức là 10 nhân lực sẽ cho ra 1 than cho mỗi click, note: Đã làm tròn trong công thức
   },
 
   // Tỉ lệ thu hoạch thụ động
